24b4e8d3d949b795b0753c58b71b5977d8008408
Spencer Tipping <spencer@spencertipping.com>
2011-10-05 07:00:54 -0600

    Lots of small fixes, a new dependency on the vector library, untested hierarchical rendering support, and bidirectional event propagation.

--- terminal.js
+++ terminal.js
@@ -56,24 +56,42 @@ caterwaul.js_all()(function ($) {
     overlay_prototype = capture [bind(name, f)    = this -se- (it['_#{name}_listeners'] = it['_#{name}_listeners'] || []).push(f),
                                  trigger(name, x) = this -se [it['_#{name}_listeners'] && it['_#{name}_listeners'] *!f[f.call(this, x)] -seq],
 
-                                 input(s)         = this /-parse_input/ s,
+                                 handle_input(s)  = this /-parse_input/ s,
 
-                                 contains(x, y)   = x >= this._position[0] && x <= this._position[0] + this._dimensions[0] &&
-                                                    y >= this._position[1] && y <= this._position[1] + this._dimensions[1]]
+                                 contains(x, y)   = x instanceof Array ? this.contains(x[0], x[1]) :
+                                                                         x >= this._position[0] && x <= this._position[0] + this._dimensions[0] &&
+                                                                         y >= this._position[1] && y <= this._position[1] + this._dimensions[1],
 
-             /events   ('click doubleclick mousedown mouseup key control'.qw)
+                                 handle(event)    = event.position(this.cursor_position()) /unless [event.position()]
+                                                    -se- this.trigger('#{event.name}_capture', event) -se- event /!this.propagate -se- this.trigger(event.name, event),
+
+                                 propagate(event) = +this %[x /~contains/ translated.position()] *![x /~handle/ translated] -seq
+                                                    -se- event.destination_is(this) /unless [it.length] -where [translated = event /~visit/ this],
+
+  // Hierarchical rendering.
+//   An overlay's render() method generates the escape codes to reload that overlay as well as its children. There are a few caveats, though. One is that this method is context-sensitive;
+//   overlays don't have parent links, so it's not possible for a single overlay to know where it is located on the screen without some extra information. The context object contains both the
+//   parent overlay and that parent's absolute position.
+
+  // The other caveat is that this method doesn't actually do anything until you provide a content() function. content() is invoked anytime render() is called, and it should return a string to
+//   redraw the overlay's contents. It also receives a copy of the context object. Note that content() is expected to deal with any existing stuff that the last render left in the terminal.
+
+                                 render(context)  = this.content(context) + +this *[x.render({parent: this, position: p})] /seq /re [it.join('')]
+                                            -where [p = this._position /-v2plus/ context.position]]
+
+             /events   ('click doubleclick mousedown mouseup key control'.qw *~![[x, '#{x}_capture']] -seq)
     /-$.merge/accessors('visible position dimensions parent cursor_position'.qw)
 
-// IO dialog.
-// The terminal sometimes writes events to standard input. Ultimately all keystroke and mouse events come in this way, and it's up to the terminal library to decode the escape sequences. These
-// are the incoming events that it understands (minus whitespace, inserted here for readability):
+  // IO dialog.
+//   The terminal sometimes writes events to standard input. Ultimately all keystroke and mouse events come in this way, and it's up to the terminal library to decode the escape sequences. These
+//   are the incoming events that it understands (minus whitespace, inserted here for readability):
 
-// | \033[ n ; m R                 <- cursor position report
-//   \033[ A-D                     <- up, down, right, left arrows, respectively
-//   \033[ M bxy                   <- mouse press event, where x and y are space-encoded numbers and b is a bitfield: 3=release, 2=button3, 1=button2, 0=button1
-//   \033[ <character>             <- alt + something, though this is ambiguous
+  // | \033[ n ; m R                 <- cursor position report
+//     \033[ A-D                     <- up, down, right, left arrows, respectively
+//     \033[ M bxy                   <- mouse press event, where x and y are space-encoded numbers and b is a bitfield: 3=release, 2=button3, 1=button2, 0=button1
+//     \033[ <character>             <- alt + something, though this is ambiguous
 
-// Everything else is either lower ASCII or a Unicode character, which is considered to be regular input.
+  // Everything else is either lower ASCII or a Unicode character, which is considered to be regular input.
 
                 -where [parse_input(overlay, s)  = overlay -se- s.split('\033[') *![overlay /-normal_keystrokes/ parse_escape(overlay, x)] -seq,
                         normal_keystrokes(o, s)  = s.split('') *![x.charCodeAt(0) < 32 ? o /-control_event/ x : o /-key_event/ x] -seq,
@@ -83,12 +101,13 @@ caterwaul.js_all()(function ($) {
                                                    /^(\d+);(\d+)R/.exec(s) -re [it ? overlay /+it[1] /-cursor_position/ +it[2]      -re- s.substr(it[0].length) :
                                                                                      overlay         /-escaped_key/     s.charAt(0) -re- s.substr(1)],
 
-                        mouse_event(o, s)        = s.charCodeAt(0) & 0x8 ? o /~mouseup/ mouse_event_for(s, 'mouseup') : o /~mousedown/ mouse_event_for(s, 'mousedown'),
+                        mouse_event(o, s)        = s.charCodeAt(0) & 0x8 ? o /~handle/ mouse_event_for(s, 'mouseup') : o /~handle/ mouse_event_for(s, 'mousedown'),
                         mouse_event_for(s, name) = $.terminal.event[name](s.charCodeAt(0) & 0x7, [s.charCodeAt(1) - 32, s.charCodeAt(2) - 32], s.charCodeAt(0) >> 4),
 
-                        arrow_event(o, s)        = o /~control/ $.terminal.event.arrow(s),
-                        control_event(o, s)      = o /~control/ $.terminal.event.control(s),
-                        escaped_key(o, s)        = o /~control/ $.terminal.event.escaped(s),
+                        arrow_event(o, s)        = o /~handle/ $.terminal.event.arrow(s),
+                        control_event(o, s)      = o /~handle/ $.terminal.event.control(s),
+                        key_event(o, s)          = o /~handle/ $.terminal.event.key(s),
+                        escaped_key(o, s)        = o /~handle/ $.terminal.event.escaped(s),
 
                         cursor_position(o, x, y) = o /~cursor_position/ [x, y]],
 
@@ -106,7 +125,7 @@ caterwaul.js_all()(function ($) {
 // | key:          fired when the user types a key that isn't a control character (basically a printable character; see below)
 //   control:      fired when the user types a control character, such as the arrow keys, Ctrl+something, Alt+something, etc.
 
-// All events come with some data that may be helpful:
+// Events come with some data that may be helpful:
 
 // | position()    the position of the event relative to the element that is receiving it. For keystrokes, the cursor position before the keystroke is used.
 //   target()      the capture destination of the event if it has been fully captured, otherwise null
@@ -117,8 +136,13 @@ caterwaul.js_all()(function ($) {
 //   button()      a bitmask of button indexes; lowest bit is 1, next is 2, next is 3
 //   modifiers()   a bitmask of modifier keys such as control, shift, and alt -- available only for mouse events. shift = bit 0, meta = bit 1, control = bit 2
 
-    event = given [data, target] [this instanceof $.terminal.event ? this -se [it._data = data, it._target = target || null] : new $.terminal.event(data, target)]
-            -se- it.prototype           /capture [stop() = this._stopped = this,  target() = this._target]
+    event = given [data, target] [this instanceof $.terminal.event ? this -se [it._data = data, it._target = target || null, it._path = []] : new $.terminal.event(data, target)]
+            -se- it.prototype           /capture [stop() = this._stopped = this,  target() = this._target,  path() = this._path,
+
+                                                  destination_is(o) = this._path *![x.target(o)] -seq -re- this,
+                                                  visit(overlay)    = new this.constructor({} /data /-$.merge/{position: this.position() /-v2minus/ overlay.position()}, target)
+                                                                      -se- it.path().push(overlay)]
+
                               /-$.merge/ accessors('position name code button arrow key modifiers'.qw)
 
             -se- it           /-$.merge/ capture [mousedown(button, position, keys)   = $.terminal.event({name: 'mousedown',   button: button, position: position, modifiers: keys}),
@@ -138,6 +162,8 @@ caterwaul.js_all()(function ($) {
 
   where [pluralize(f)(xs)  = xs *[[x, f(x)]] -object -seq,
          accessor(name)(x) = arguments.length ? this -se [it['_#{name}'] = x] : this['_#{name}'],  accessors = pluralize(accessor),
-         event(name)(x)    = x instanceof Function ? this.bind(name, x) : this.trigger(name, x),   events    = pluralize(event)]})(caterwaul);
+         event(name)(x)    = x instanceof Function ? this.bind(name, x) : this.trigger(name, x),   events    = pluralize(event)],
+
+  using [caterwaul.linear.vector(2, 'v2')]})(caterwaul);
 
 // Generated by SDoc 
